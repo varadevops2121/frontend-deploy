84bf6606980dd158c8e76114b2f79ffcec92b4e1
varalaxmi <varadevops2121@gmail.com>
2024-12-05 03:56:21 +0530

    frontend-deploy

---
+++ terraform/variables.tf
@@ -0,0 +1,26 @@
+
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Environment = "dev"
+    Terraform = "true"
+    Component = "frontend"
+  }
+}
+
+variable "zone_name" {
+  default = "lerndevops.online"
+}
+
+#created this as part of Jenkins CD
+variable "app_version" {
+  
+}
